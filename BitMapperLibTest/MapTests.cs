db29b75d1b181d62e2a8822ee40f9ff81e4d2a6a
altacus <altacus@x13s-debian>
2026-02-06 16:24:29 -0500

    fixes

--- BitMapperLibTest/MapTests.cs
+++ BitMapperLibTest/MapTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using BitMapperLib;
 namespace BitMapperLibTest;
 
 public class MapTests
@@ -6,29 +7,40 @@ public class MapTests
     [Test]
     public void SetsTargetBit_WhenSourceBitIsSet()
     {
-        var sut = new BitMapperLib.Class1();
+        var sut = new Class1();
         int result = sut.Map(1); // source bit 0 set
-        int expected = 1 << sut.mapping[0];
+        int expected = 1 << Class1.mapping[0];
         Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void ClearsTargetBit_WhenSourceBitIsNotSet()
     {
-        var sut = new BitMapperLib.Class1();
-        int valueWithTargetSet = 1 << sut.mapping[0]; // target bit set, corresponding source (0) not set
+        var sut = new Class1();
+        int valueWithTargetSet = 1 << Class1.mapping[0]; // target bit set, corresponding source (0) not set
         int result = sut.Map(valueWithTargetSet);
-        int targetBitMask = 1 << sut.mapping[0];
+        int targetBitMask = 1 << Class1.mapping[0];
         Assert.That((result & targetBitMask), Is.EqualTo(0));
     }
 
     [Test]
-    public void ClearsTargetBit_custom()
+    public void ClearsTargetBit_customMap()
     {
-        var sut = new BitMapperLib.Class1();
+        var sut = new Class1();
         int valueWithTargetSet = 1 ; // target bit set, corresponding source (0) not set
         int result = sut.Map(valueWithTargetSet);
         
-        Assert.That(result, Is.EqualTo(Math.Pow(2, sut.mapping[0])));
+        Assert.That(result, Is.EqualTo(Math.Pow(2, Class1.mapping[0])));
+    }
+
+    [Test]
+    public void ClearsTargetBit_customUnmap()
+    {
+        var sut = new Class1();
+        int valueWithTargetSet = 189482188;
+        int result = sut.Map(valueWithTargetSet);
+        int resultUnmapped = sut.Unmap(result);
+        
+        Assert.That(resultUnmapped, Is.EqualTo(valueWithTargetSet));
     }
 }
