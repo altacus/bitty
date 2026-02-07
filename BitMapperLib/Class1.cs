a7746c4628697c4a0732e1191cf40057a8a54f76
altacus <altacus@x13s-debian>
2026-02-06 22:28:10 -0500

    refactoring

--- BitMapperLib/Class1.cs
+++ BitMapperLib/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Numerics;
 
 namespace BitMapperLib;
@@ -6,35 +7,25 @@ namespace BitMapperLib;
 public class Class1
 {
     /* Hard coded values for where each source bit (index) maps to a target bit */
-    public static short[] mapping = [27, 3, 14, 0, 30, 8, 11, 22, 5, 17, 29, 1, 19, 12, 25, 7, 31, 10, 16, 2, 21, 15, 6, 28, 9, 20, 18, 4, 13, 23, 26, 24];
+    public static readonly short[] Mapping = [27, 3, 14, 0, 30, 8, 11, 22, 5, 17, 29, 1, 19, 12, 25, 7, 31, 10, 16, 2, 21, 15, 6, 28, 9, 20, 18, 4, 13, 23, 26, 24];
 
-    private readonly uint[] targetMasks;
-    private readonly uint[] sourceMasks;
+    private static readonly uint[] targetMasks = [.. Enumerable.Range(0, Mapping.Length).Select(i => 1u << Mapping[i])];
 
-    public Class1()
+    private static readonly uint[] sourceMasks = BuildSourceMasks();
+
+    private static uint[] BuildSourceMasks()
     {
-        targetMasks = new uint[mapping.Length];
-        for (int i = 0; i < mapping.Length; i++)
+        var masks = new uint[Mapping.Length];
+        for (int i = 0; i < Mapping.Length; i++)
         {
-            targetMasks[i] = 1u << mapping[i];
+            masks[Mapping[i]] = 1u << i;
         }
-        sourceMasks = new uint[mapping.Length];
-        for (int i = 0; i < mapping.Length; i++)
-        {
-            // mapping[i] is the target for source i, so set the source mask at the target index
-            sourceMasks[mapping[i]] = 1u << i;
-        }
+        return masks;
     }
 
-    public int Map(int value)
-    {
-        return (int)ApplyMasks((uint)value, targetMasks);
-    }
+    public static int Map(int value) => (int)ApplyMasks((uint)value, targetMasks);
 
-    public int Unmap(int mappedValue)
-    {
-        return (int)ApplyMasks((uint)mappedValue, sourceMasks);
-    }
+    public static int Unmap(int mappedValue) => (int)ApplyMasks((uint)mappedValue, sourceMasks);
 
     private static uint ApplyMasks(uint v, uint[] masks)
     {
--- BitMapperLibTest/MapTests.cs
+++ BitMapperLibTest/MapTests.cs
@@ -8,8 +8,8 @@ public class MapTests
     public void SetsTargetBit_WhenSourceBitIsSet()
     {
         var sut = new Class1();
-        int result = sut.Map(1); // source bit 0 set
-        int expected = 1 << Class1.mapping[0];
+        int result = Class1.Map(1); // source bit 0 set
+        int expected = 1 << Class1.Mapping[0];
         Assert.That(result, Is.EqualTo(expected));
     }
 
@@ -17,9 +17,9 @@ public class MapTests
     public void ClearsTargetBit_WhenSourceBitIsNotSet()
     {
         var sut = new Class1();
-        int valueWithTargetSet = 1 << Class1.mapping[0]; // target bit set, corresponding source (0) not set
-        int result = sut.Map(valueWithTargetSet);
-        int targetBitMask = 1 << Class1.mapping[0];
+        int valueWithTargetSet = 1 << Class1.Mapping[0]; // target bit set, corresponding source (0) not set
+        int result = Class1.Map(valueWithTargetSet);
+        int targetBitMask = 1 << Class1.Mapping[0];
         Assert.That((result & targetBitMask), Is.EqualTo(0));
     }
 
@@ -28,18 +28,17 @@ public class MapTests
     {
         var sut = new Class1();
         int valueWithTargetSet = 1 ; // target bit set, corresponding source (0) not set
-        int result = sut.Map(valueWithTargetSet);
+        int result = Class1.Map(valueWithTargetSet);
         
-        Assert.That(result, Is.EqualTo(Math.Pow(2, Class1.mapping[0])));
+        Assert.That(result, Is.EqualTo(Math.Pow(2, Class1.Mapping[0])));
     }
 
     [Test]
     public void ClearsTargetBit_customUnmap()
     {
-        var sut = new Class1();
         int valueWithTargetSet = 189482188;
-        int result = sut.Map(valueWithTargetSet);
-        int resultUnmapped = sut.Unmap(result);
+        int result = Class1.Map(valueWithTargetSet);
+        int resultUnmapped = Class1.Unmap(result);
         
         Assert.That(resultUnmapped, Is.EqualTo(valueWithTargetSet));
     }
